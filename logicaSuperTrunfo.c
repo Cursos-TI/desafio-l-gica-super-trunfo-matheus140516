7885288384f5b257d3d46688ea7e1d84a8129da4
Matheus.F.Abreu <143409802+matheus140516@users.noreply.github.com>
2025-03-27 17:51:13 +0000

    27/03/2025

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,10 +1,46 @@
 #include <stdio.h>
 
+// Função principal
 int main() {
-    // Declaração das variáveis para armazenar os dados das cartas
-    char estado1, estado2;
-    char codigo1[5], codigo2[5];
-    char nomeCidade1[50], nomeCidade2[50];
-    int populacao1, populacao2;
-    float area1, area2, pib1, pib2;
-    int pontosTuristicos1, pontosTuristicos2;
+    // Cadastro das duas cartas do Super Trunfo
+    char estado1[] = "São Paulo";
+    char codigo1[] = "SP01";
+    char cidade1[] = "São Paulo";
+    int populacao1 = 12300000;
+    float area1 = 1521.11;
+    float pib1 = 699000000000; // PIB em bilhões
+    int pontosTuristicos1 = 15;
+
+    char estado2[] = "Rio de Janeiro";
+    char codigo2[] = "RJ01";
+    char cidade2[] = "Rio de Janeiro";
+    int populacao2 = 6000000;
+    float area2 = 1200.27;
+    float pib2 = 400000000000; // PIB em bilhões
+    int pontosTuristicos2 = 10;
+
+    // Cálculos: Densidade Populacional e PIB per capita
+    float densidadePopulacional1 = populacao1 / area1;
+    float pibPerCapita1 = pib1 / populacao1;
+
+    float densidadePopulacional2 = populacao2 / area2;
+    float pibPerCapita2 = pib2 / populacao2;
+
+    // Escolha direta do atributo para comparação: População neste caso
+    char atributoEscolhido[] = "População";
+
+    printf("Comparação de cartas (Atributo: %s):\n\n", atributoEscolhido);
+    printf("Carta 1 - %s (%s): %d\n", cidade1, estado1, populacao1);
+    printf("Carta 2 - %s (%s): %d\n\n", cidade2, estado2, populacao2);
+
+    // Lógica de comparação para determinar o vencedor
+    if (populacao1 > populacao2) {
+        printf("Resultado: Carta 1 (%s) venceu!\n", estado1);
+    } else if (populacao2 > populacao1) {
+        printf("Resultado: Carta 2 (%s) venceu!\n", estado2);
+    } else {
+        printf("Resultado: Empate! Ambas as cartas têm a mesma população.\n");
+    }
+
+    return 0;
+}
